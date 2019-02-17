16cf804359aa9f637dde9fee98ba826a7282ea86
Lloyd Napata <29262019+loynaps@users.noreply.github.com>
2019-02-17 09:14:28 +0200

    initial  sketch.js file

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+
+}
+
+/*This line of code means "draw an ellipse, with the center 50 pixels over from the left and 50 pixels down from the top, with a width and height of 80 pixels". 
+ */
+function draw() {
+    ellipse(50, 50, 80, 80);
+}
